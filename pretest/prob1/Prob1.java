34f270afd2a3012b3234084ee800475d59b37cf2
John Sampson <jsamps100@uafs.edu>
2026-08-20 19:42:46 -0500

    Update

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -2,8 +2,12 @@ package prob1;
 
 public class Prob1 {
 
+	public static int FindMaxDifference(int[], numbers) {
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		
 
 	}
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -1,8 +1,33 @@
+//CharAt Length Equals
+
 package prob2;
 
+import java.util.Scanner;
+
 public class Prob2 {
+	
+	public static int countVowelSequences(String str) {
+		int vowels = 0;
+		int totalPairs = 0;
+		
+		if(vowels == 2)
+			totalPairs = totalPairs + 1;
+			vowels = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+			vowels = vowels + 1;
+			}
+		}
+		return totalPairs;
+	}
 
 	public static void main(String[] args) {
+		
+		Scanner console = new Scanner(System.in);
+		
+		String str = console.next(); 
+		
+		System.out.println(countVowelSequences(str));
 		
 
 	}
